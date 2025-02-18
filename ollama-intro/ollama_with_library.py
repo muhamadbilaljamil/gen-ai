aca5bdfb0d508668525ec1f0a07c0a4895adb87a
Muhammad Bilal Jamil <muhamadbilaljamil@gmail.com>
2025-02-18 19:02:27 +0500

    working on custom model

--- ollama-intro/ollama_with_library.py
+++ ollama-intro/ollama_with_library.py
@@ -8,7 +8,7 @@ client = ollama.Client()
 
 # Define the model and the input prompt
 model = "llama3.2"
-prompt = "How i can start my blog to earn money?"
+prompt = "Can i concat my nextjs application into wordpres?"
 
 # Send the query to the model with streaming enabled
 response = client.generate(model, prompt, stream=True)
